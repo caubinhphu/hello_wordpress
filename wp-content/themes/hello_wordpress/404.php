a61d3b01c09ace388c1198532196539b61c9b806
Anh Hai <10a3nguyenanhhai@gmail.com>
2020-09-24 11:50:22 +0700

    add 404.php

---
+++ wp-content/themes/hello_wordpress/404.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("images/ocean.jpg") ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">404: Page not found</h1>
+  </div>  
+</div>
+
+<?php get_footer() ?>
