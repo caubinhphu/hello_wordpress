f8e0dcc6bcdadc7539834917872aa0dbfaea67eb
Anh Hai <10a3nguyenanhhai@gmail.com>
2020-09-24 10:14:39 +0700

    custom comment

--- wp-content/themes/hello_wordpress/functions.php
+++ wp-content/themes/hello_wordpress/functions.php
@@ -3,7 +3,7 @@
 function load_file() {
   wp_enqueue_style( "google-font", "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
   wp_enqueue_style( "font-awesome", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-  wp_enqueue_style( "style", get_stylesheet_uri());
+  wp_enqueue_style( "style", get_stylesheet_uri(), array(), rand(1, 99), 'all');
   wp_enqueue_script( "script", get_template_directory_uri() . "/js/scripts-bundled.js", NULL, '1.0', true );
 }
 
@@ -21,3 +21,13 @@ function mytheme_custom_excerpt_length( $length ) {
 }
 
 add_filter( 'excerpt_length', 'mytheme_custom_excerpt_length', 999);
+
+
+function wpbeginner_comment_text_after($arg) {
+  $arg['comment_notes_after'] = "<p class='comment-policy'>We are glad you have chosen to leave a comment. Please keep in mind that comments are moderated according to our <a href='#'>comment policy</a>.</p>";
+  $arg['class_submit'] = "submit-comment";
+  $arg['label_submit'] = 'SUBMIT COMMENT';
+  return $arg;
+}
+
+add_filter('comment_form_defaults', 'wpbeginner_comment_text_after');
--- wp-content/themes/hello_wordpress/single.php
+++ wp-content/themes/hello_wordpress/single.php
@@ -17,9 +17,55 @@
 
 </div>
 
-<?php the_field('custom_field') ?>
+<div style="text-align: right; padding-right: 20px;">
+  <em>View: <?php the_field('view') ?></em>
+</div>
+
 
 <div class="page-section page-section--white"></div>
-<?php get_comments(  ); ?>
-<?php comment_form(); ?>
+
+<div class="comment-area">
+  <?php
+    $comments = get_comments(array(
+      'post_id' => $post->ID,
+      'status' => 'approve'
+    ));
+    $count = count($comments);
+  ?>
+
+  <div><?php echo $count === 1 ? "$count COMMENT" : "$count COMMENTS"; ?></div>
+
+  <div class="comment-list">
+    <?php
+      foreach($comments as $comment) {
+        ?>
+        <div class="comment-item">
+          <div class="comment-item-top">
+            <img class="comment-avatar"
+              src="<?php echo get_avatar_url($comment->comment_author_email); ?>"
+              alt="<?php echo $comment->comment_author ?>"
+            >
+            <div>
+              <div>
+                <strong>
+                  <?php echo $comment->comment_author ?>
+                </strong>
+              </div>
+              <div>
+                <?php echo $comment->comment_date ?>
+              </div>
+            </div>
+          </div>
+          <div class="comment-item-bottom">
+            <div class="comment-content">
+              <?php echo $comment->comment_content ?>
+            </div>
+          </div>
+        </div>
+    <?php } ?>
+  </div>
+  
+  <?php comment_form(); ?>
+</div>
+
 <?php get_footer(); ?>
